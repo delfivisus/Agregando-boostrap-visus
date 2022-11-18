a6fa84f248a896b30bafb9bdd9737f5beb056638
delfina <visusdelfi@gmail.com>
2022-11-18 14:27:16 -0300

    Incorpore carrito

--- index.js
+++ index.js
@@ -1,3 +1,134 @@
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    fetchData()
+    if(localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem ('carrito'))
+        pintarCarrito()
+    }
+})
+
+cards.addEventListener('click', e => {
+    addCarrito(e)
+})
+
+items.addEventListener('click', e => {
+    btnAccion(e)
+})
+
+const fetchData = async () => {
+    try{
+        const res = await fetch('data.json')
+        const data= await res.json()
+        pintarCards(data)
+    }catch (error) {
+        console.log(error)
+    }
+}
+
+const pintarCards = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.nombre 
+        templateCard.querySelector('p').textContent = producto.precio
+        templateCard.querySelector('img').setAttribute("src", producto.img)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+        
+    })
+    cards.appendChild(fragment)
+}
+
+const addCarrito = e => {
+    if (e.target.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        nombre: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad:1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1 
+    }
+    carrito[producto.id] = {...producto}
+    pintarCarrito ()
+}
+
+const pintarCarrito = () => {
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id 
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    pintarFooter()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+        footer.innerHTML `
+        <th scope="row" colspan="5">Carrito vacio - comience a comprar!</th>
+        `
+        return
+    }
+    const nCantidad = Object.values(carrito).reduce((acc,{cantidad}) => acc + cantidad,0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
+
+    templateFooter.querySelectorAll('td') [0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const btnVaciar = document.getElementById('vaciar-carrito')
+    btnVaciar.addEventListener('click', () => {
+        carrito = []
+        pintarCarrito()
+    })
+}
+
+const btnAccion = e => {
+    if (e.target.classList.contains('btn-info')){
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = {...producto}
+        pintarCarrito()
+    }
+    if (e.target.classList.contains('btn-danger')){
+        producto.cantidad--
+        if(producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        }
+    }
+    e.stopPropagation() 
+}
+
 (async () => {
     const {value: localidad} = await Swal.fire({
 });
@@ -40,80 +171,3 @@ Swal.fire({
     }
 
 })
-
-
-const traerProductos = async () => {
-    const listado = document.getElementById("listado");
-    const response = await fetch("./data.json");
-    const data = await response.json();
-    
-    data.forEach((producto) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
-        <h2>${producto.nombre}</h2>
-        <p>${producto.precio}</p>
-        <p>${producto.id}</p>
-        <hr/>
-    `;
-        listado.append(li);
-    });
-};
-
-traerProductos();
-
-const div = document.getElementById("div")
-const boton =document.getElementById("boton")
-let productos = [
-    {
-        id= 1,
-        articulo:"Bolsa Pedigree",
-        imagen:"https://http2.mlstatic.com/D_NQ_NP_813120-MLA49656683084_042022-V.jpg",
-        precio: 8500,
-
-    }
-    {
-        id= 2,
-        articulo:"Bolsa Dogchow",
-        imagen:"https://http2.mlstatic.com/D_NQ_NP_793926-MLA43229058650_082020-O.webp",
-        precio: 10500,
-    } 
-]
-
-let carrito = []
-
-
-productos.forEach(producto => {
-    let productoRenderizado = document.createElement("div")
-    productoRenderizado.innerHTML = 
-    <h3>articulo:${producto.articulo}</h3>
-    <img src= "${producto.imagen}">
-    <span>precio: $${producto.precio}</span>
-    <button id=${producto.id}>Comprar</button>
-    
-    const boton = document.getElementById(producto.id)
-    div.append(productoRenderizado)
-    boton.addEventListener("click", () => comprarProducto(producto))
-});
-
-const comprarProducto = (producto) => {
-    let productoExiste = carrito.find(item => item.id === producto.id)
-    if(productoExiste === undefined){
-        carrito.push({
-            id: producto.id,
-            articulo: producto.articulo,
-            imagen: producto.imagen,
-            precio: producto.precio,
-            cantidad: 1
-        })
-    }else{
-        productoExiste.precio = productoExiste.precio + producto.precio
-        productoExiste.cantidad = productoExiste,cantidad + 1
-    }
-
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-localStorage.setItem("registro", JSON.stringify(usuarios))
-let returnCarrito = JSON.parse(localStorage.getItem("carrito"))
-
-boton.addEventListener("click", () => console.log(carrito))
- */</img>
